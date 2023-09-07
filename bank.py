08b3d11e9d051f36ccf2bb4d5ec26b71249fdfa4
Aly <aly_alidina@hotmail.com>
2023-09-07 20:57:15 +0000

    Thu, Sep 7, 2023, 8:57 PM +00:00

--- bank.py
+++ bank.py
@@ -1,7 +1,7 @@
 input = input("Greeting:").lower()
 if input == "hello":
-    print("$0)
-elif:
-    input[0] == "h"
+    print("$0")
+elif input[0] == "h":
+     print("$20")
 else:
     print("$100")
