4823ed52a0d60ae65d25b94c76cbc047eb0ecf51
Aly <aly_alidina@hotmail.com>
2023-09-07 20:45:56 +0000

    Thu, Sep 7, 2023, 8:45 PM +00:00

--- deep.py
+++ deep.py
@@ -1,5 +1,5 @@
 input = input("What is the answer to the Great Question of Life, the Universe and Everything, outputting ").lower().strip()
-if input == "42" or input == "fortytwo" or input == "forty two" or input == "forty-two":
+if input == "42" or input == "forty two" or input == "forty-two":
     print("Yes")
 else:
     print("No")
