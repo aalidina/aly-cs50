c47eec7961f1f472746d877aa3de62f1467e6e89
Aly <aly_alidina@hotmail.com>
2023-09-08 21:05:46 +0000

    Fri, Sep 8, 2023, 5:05 PM -04:00

--- interpreter.py
+++ interpreter.py
@@ -1,15 +1,15 @@
-interpreter = (input("Expression: " ).split(" ")
+interpreter = input("Expression: " ).split(" ")
 
-x, y, z = calculator
+x, y, z = interpreter
 
 a = float(x)
 b = float(b)
 
-if y == +:
+if y == "+":
     print(a + b)
-elif y[1] in interpreter:
-    print(x - z)
-elif y[2] in interpreter:
-    print(x + z)
+# elif y[1] in interpreter:
+#     print(x - z)
+# elif y[2] in interpreter:
+#     print(x + z)
 else:
     print(x * z)
