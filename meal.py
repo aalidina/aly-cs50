fc60e89bb43f5cd140dbe8f585cad95d3722c688
Aly <aly_alidina@hotmail.com>
2023-09-11 18:11:22 +0000

    Mon, Sep 11, 2023, 2:11 PM -04:00

--- meal.py
+++ meal.py
@@ -4,9 +4,9 @@ def main():
     breakfast = "7:00 and 8:00"
     lunch = "12:00 and 13:00"
     dinner = "18:00 and 19:00"
-    
 
-    if time > 7 && time < 8:
+
+    if time > 7 & time < 8:
         print("breakfast time")
 
     print(time)
