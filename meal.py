7bc196326913a7b809f849f7970265643527464c
Aly <aly_alidina@hotmail.com>
2023-09-11 18:15:44 +0000

    Mon, Sep 11, 2023, 2:15 PM -04:00

--- meal.py
+++ meal.py
@@ -2,15 +2,15 @@ def main():
     time = input("What time is it? ").split(":")
 
     h, m = time
-    a = float(a)
-    b = float(b)
+    a = float(h)
+    b = float(m)
 
     breakfast = "7:00 and 8:00"
     lunch = "12:00 and 13:00"
     dinner = "18:00 and 19:00"
 
 
-    if time > 7 & time < 8:
+    if a+b > 7 & a+b < 8:
         print("breakfast time")
 
     print(time)
