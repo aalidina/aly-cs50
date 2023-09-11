6685708ae703b416be096d9b90e7a47ccc21b7f7
Aly <aly_alidina@hotmail.com>
2023-09-11 18:04:21 +0000

    Mon, Sep 11, 2023, 2:04 PM -04:00

--- meal.py
+++ meal.py
@@ -1,10 +1,12 @@
 def main():
-    time = input(" Enter Time: ").split(":")
+    time = input("What time is it? ").split(":")
 
     breakfast time = "7:00 and 8:00"
     lunch time = "12:00 and 13:00"
     dinner time = "18:00 and 19:00"
-    
+
+    if time 
+
     print(time)
 
 
