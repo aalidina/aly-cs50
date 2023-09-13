0c05fdcea3269d699373ac7b9cb06305d2a63cc7
Aly <aly_alidina@hotmail.com>
2023-09-13 17:28:59 +0000

    Wed, Sep 13, 2023, 1:28 PM -04:00

--- meal.py
+++ meal.py
@@ -3,8 +3,10 @@
 def main():
     x = input("What time is it? ")
     x = convert(x)
-    if 7<=x<=8:
+    if 7 <= x <=8:
         print("breakfast time")
+    elif 12 <= x <= 13
+        print("Lunch Time")
 
 
 
@@ -13,14 +15,13 @@ def convert(time):
 # convert is a function (that can be called by main) that converts time, a str in 24-hour format,
 # to the corresponding number of hours as a float. For instance, given a time like "7:30" (i.e., 7 hours and 30 minutes), convert should return 7.5 (i.e., 7.5 hours).
 
-    h, m = time
+    h, m = time.split(":")
 
     a = float(h)
     b = float(m)
 
-
-    hours = a + b / 60
-    return(hours)
+    time = a + b / 60
+    return(time)
     # print(hours)
 
 
