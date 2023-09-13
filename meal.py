e96596a8cb664d6d66d1132cdd79ac6333a6d9fc
Aly <aly_alidina@hotmail.com>
2023-09-13 16:09:07 +0000

    Wed, Sep 13, 2023, 12:09 PM -04:00

--- meal.py
+++ meal.py
@@ -4,6 +4,7 @@
 
 def main():
     time = input("What time is it? ")
+
     # h, m = timetime.split(":")
     # hours = float(h * 24)
     # print(hours)
@@ -19,13 +20,7 @@ def main():
 
 def convert(time):
 
-    #   h, m = timetime.split(":")
-    #   hours = float(h * 24)
-    #   print(hours)
-
-# breakfast = "7:00 and 8:00"
-# lunch = "12:00 and 13:00"
-# dinner = "18:00 and 19:00"
+   
 
 
 if __name__ == "__main__":
