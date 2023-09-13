a52bc8a84d5b2d5ce76d56a3eb6b57d483c866e1
Aly <aly_alidina@hotmail.com>
2023-09-13 15:47:41 +0000

    Wed, Sep 13, 2023, 11:47 AM -04:00

--- meal.py
+++ meal.py
@@ -1,3 +1,7 @@
+
+# Structure your program per the below, wherein convert is a function (that can be called by main) that converts time, a str in 24-hour format,
+# to the corresponding number of hours as a float. For instance, given a time like "7:30" (i.e., 7 hours and 30 minutes), convert should return 7.5 (i.e., 7.5 hours).
+
 def main():
     time = input("What time is it? ").split(":")
 
