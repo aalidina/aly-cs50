402aab528aa8b53a656ca9c955cd68ebdb433f6a
Aly <aly_alidina@hotmail.com>
2023-09-15 18:23:16 +0000

    Fri, Sep 15, 2023, 2:23 PM -04:00

--- coke.py
+++ coke.py
@@ -1,5 +1,4 @@
 def main():
-    c = 0
     d = [5, 10, 25]
     print("Amout Due: 50")
     while c < 50:
@@ -11,6 +10,7 @@ def main():
             print("Amount Due:", 50)
 
 def change():
+    c = 0
     if c > 50:
         print("Change Owed:", c - 50)
     elif c == 50:
