b08f4da3ce1d5cf5d276c8d135ec09754f18a17f
Aly <aly_alidina@hotmail.com>
2023-09-15 18:02:03 +0000

    Fri, Sep 15, 2023, 2:02 PM -04:00

--- coke.py
+++ coke.py
@@ -1,13 +1,15 @@
 c = 0
-total = 0
+d = [5, 10, 25]
 print("Amout Due: 50")
 while c < 50:
     coin = int(input("Insert Coin: "))
-    c = coin + c
-    if c > 50:
-        print("Change Owed:", c - 50)
-    elif c == 50:
-         print("Change Owed:", 0)
-    else:
-        print("Amount Due:", 50 - c)
+    if coin in d:
+        c = coin + c
+
+        if c > 50:
+            print("Change Owed:", c - 50)
+        elif c == 50:
+            print("Change Owed:", 0)
+        else:
+            print("Amount Due:", 50 - c)
 
