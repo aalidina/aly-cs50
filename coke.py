257a5029f34e5756800e3e2275033124ed54115f
Aly <aly_alidina@hotmail.com>
2023-09-15 17:53:14 +0000

    Fri, Sep 15, 2023, 1:53 PM -04:00

--- coke.py
+++ coke.py
@@ -1,8 +1,8 @@
 c = 0
 total = 0
+print("Amout Due: 50")
 while c < 50:
-    print("Amout Due: 50")
-    coin = int(input("Incert Coin: "))
-    print("Amount Due )
+    coin = int(input("Insert Coin: "))
     c = coin + c
+    print("Amount Due:", 50 - c)
 
