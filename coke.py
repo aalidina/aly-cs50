cb4be75f069799a89cd8ffedbeb64859704681af
Aly <aly_alidina@hotmail.com>
2023-09-15 18:26:06 +0000

    Fri, Sep 15, 2023, 2:26 PM -04:00

--- coke.py
+++ coke.py
@@ -1,16 +1,17 @@
 def main():
+    c = 0
     d = [5, 10, 25]
     print("Amout Due: 50")
     while c < 50:
         coin = int(input("Insert Coin: "))
         if coin in d:
-            change()
+            c = coin + c
+            change(c)
 
         else:
             print("Amount Due:", 50)
 
-def change():
-    c = 0
+def change(c):
     if c > 50:
         print("Change Owed:", c - 50)
     elif c == 50:
