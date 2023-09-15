4a2f24aba640147817d9c07844c22ab3b93a78a4
Aly <aly_alidina@hotmail.com>
2023-09-15 20:35:56 +0000

    Fri, Sep 15, 2023, 4:35 PM -04:00

--- twttr.py
+++ twttr.py
@@ -2,5 +2,6 @@ vowels = ["a","e","i","o","u"]
 input = input("Input: ")
 for i in input:
     if i in vowels:
-        print(i.lstrip(vowels), end="")
+        x = i.lstrip()
+        print(x, end="")
 print(" ")
