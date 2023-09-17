8192dce5cf8ab0f84ff7b2a174c385668f604fa7
Aly <aly_alidina@hotmail.com>
2023-09-17 18:31:12 +0000

    Sun, Sep 17, 2023, 2:31 PM -04:00

--- twttr.py
+++ twttr.py
@@ -3,6 +3,6 @@ input = input("Input: ")
 for i in input:
     if i in vowels:
         x = str(i.lstrip())
-        input.translate( { ord(x): None } ), end=""
+        input.translate( { ord(x): None } ), end="")
         print(y)
 print(" ")
