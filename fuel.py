4238f21605f974d6b035c7642f6b50d292558135
Aly <aly_alidina@hotmail.com>
2023-09-27 18:31:59 +0000

    Wed, Sep 27, 2023, 2:31 PM -04:00

--- fuel.py
+++ fuel.py
@@ -11,7 +11,7 @@ def fuel_gauge():
             a = float(x)
             b = float(y)
 
-            if a/b * 100 = 0:
+            if a/b * 100 == 0:
                 print("E")
             elif a/b * 100 == 1:
                 print("F")
@@ -23,7 +23,7 @@ def fuel_gauge():
         except ValueError:
             print("x is not an integer")
         else:
-            return fuel
+            return "false"
 
 
 
