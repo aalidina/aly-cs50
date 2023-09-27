d27e500ec04c9678158ebfdc2c74dcb8b3b20785
Aly <aly_alidina@hotmail.com>
2023-09-27 18:10:33 +0000

    Wed, Sep 27, 2023, 2:10 PM -04:00

--- fuel.py
+++ fuel.py
@@ -4,18 +4,23 @@ def main():
     print(f"{g}")
 
 def fuel_gauge():
-    try:
-        fuel = input("Enter Fraction: ").split("/")
-        x,y = fuel
-        a = float(x)
-        b = float(y)
+    while True:
+        try:
+            fuel = input("Enter Fraction: ").split("/")
+            x,y = fuel
+            a = float(x)
+            b = float(y)
 
-        if a/b * 100 <= 0.01:
-            return "E"
-        elif a/b * 100 >= 0.99:
-            return "F"
-    except ValueError:
-         pass
+            if a/b * 100 <= 0.01:
+                return "E"
+            elif a/b * 100 >= 0.99:
+                return "F"
+        except ValueError:
+            print("x is not an integer")
+        else:
+            break
+
+    return fuel
 
 
 
