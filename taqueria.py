bc30b1839b41d456166e91ee5f47ea0ec6aeaaa0
Aly <aly_alidina@hotmail.com>
2023-09-28 21:55:34 +0000

    Thu, Sep 28, 2023, 5:55 PM -04:00

--- taqueria.py
+++ taqueria.py
@@ -13,10 +13,10 @@ menu = {
 
 # while True:
 try:
-item = input("Item: ").lower()
-for i in menu:
-if item == i.lower():
-print("$" + f"{menu[i]}")
+    item = input("Item: ").lower()
+    for i in menu:
+        if item == i.lower():
+            print("$" + f"{menu[i]}")
 except EOFError:
     pass
 else:
