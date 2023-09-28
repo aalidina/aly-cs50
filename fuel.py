8523bb63fce6a5dee1d309b172eedfb26aa972e3
Aly <aly_alidina@hotmail.com>
2023-09-28 15:45:58 +0000

    Thu, Sep 28, 2023, 11:45 AM -04:00

--- fuel.py
+++ fuel.py
@@ -18,6 +18,10 @@ def fuel_gauge(prompt):
                 return "E"
             elif r  == 1 or r == 99/100:
                 return "F"
+            elif r == 75:
+                return "75%"
+            elif r == 75:
+                return "75%"
             print(round(r * 100), '%', sep='')
             break
 
