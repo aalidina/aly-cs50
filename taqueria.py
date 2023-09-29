4ac4be57b19fbf468a890257853db23b2dc95cd2
Aly <aly_alidina@hotmail.com>
2023-09-29 13:14:24 +0000

    Fri, Sep 29, 2023, 9:14 AM -04:00

--- taqueria.py
+++ taqueria.py
@@ -9,20 +9,10 @@ menu = {
     "Taco": "3.00",
     "Tortilla Salad": "8.00"
 }
-def main():
-    x = menu()
-    return x
-
-def menu():
-    while True:
-        try:
-            item = input("Item: ").lower()
-            for i in menu:
-                if item == i.lower():
-                    print("$" + f"{menu[i]}")
-        except EOFError:
-            pass
-        else:
-            return "x"
 
 
+
+item = input("Item: ").lower()
+for i in menu:
+    if item == i.lower():
+        print("$" + f"{menu[i]}")
