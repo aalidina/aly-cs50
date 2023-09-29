3168587390697df56697486815146d34dd7911df
Aly <aly_alidina@hotmail.com>
2023-09-29 20:16:16 +0000

    Fri, Sep 29, 2023, 4:16 PM -04:00

--- test.py
+++ test.py
@@ -1,19 +1,44 @@
-menu = {
-    "Baja Taco": 4.00,
-    "Burrito": 7.50,
-    "Bowl": 8.50,
-    "Nachos": 11.00,
-    "Quesadilla": 8.50,
-    "Super Burrito": 8.50,
-    "Super Quesadilla": 9.50,
-    "Taco": 3.00,
-    "Tortilla Salad": 8.00
+# ////////////////////////////////////////
+# //                                    //
+# // Name: Tristan Simpson              //
+# //                                    //
+# // Assignment: Felipe's Taqueria      //
+# //                                    //
+# ////////////////////////////////////////
+
+# // Store the menu items and the cost as values
+MENU: dict[str, float] = {
+    "baja taco": 4.00,
+    "burrito": 7.50,
+    "bowl": 8.50,
+    "nachos": 11.00,
+    "quesadilla": 8.50,
+    "super burrito": 8.50,
+    "super quesadilla": 9.50,
+    "taco": 3.00,
+    "tortilla salad": 8.00
 }
 
+# // Store the total cost
+total: int = 0
 
+while 1:
+    try:
+        # // Get the item
+        item: str = input("Item: ").lower()
 
-item = input("Item: ")
-if item in menu:
-    print(menu[item])
-else:
-    print("x")
+        # // Increase the total by the value
+        # // from the item in the MENU map
+        if item in MENU:
+            total += MENU[item]
+
+        # // Print the new total formatted to 2 decimal points
+        print(f"Total: ${total:.2f}")
+
+    # // Except Control+D is pressed
+    except EOFError:
+        break
+
+# https://github.com/settings/tokens
+# check50 cs50/problems/2022/python/taqueria
+# submit50 cs50/problems/2022/python/taqueria
