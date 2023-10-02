9d211dff78de1789e3011aa47d8c88eef4023aff
Aly <aly_alidina@hotmail.com>
2023-10-02 16:02:38 +0000

    Mon, Oct 2, 2023, 12:02 PM -04:00

--- grocery.py
+++ grocery.py
@@ -1,10 +1,10 @@
-grocery = ""
+grocery = []
 while True:
     try:
         items = input("Enter Items: ").upper()
         grocery += items
     except EOFError:
         # for i in grocery:
-        print(grocery, end="\n")
+        print(grocery, sep=(","))
         break
 
