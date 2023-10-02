96dec88a8778024574fb11f3e53484dfeaa854b4
Aly <aly_alidina@hotmail.com>
2023-10-02 20:44:48 +0000

    Mon, Oct 2, 2023, 4:44 PM -04:00

--- grocery.py
+++ grocery.py
@@ -4,12 +4,11 @@ while True:
     try:
         items = input("Enter Items: ").upper()
         grocery.append(items)
-
-    except EOFError:
         for i in grocery:
             print(i, end='\n')
-            print(grocery)
-            break
+    except EOFError:
+        break
+    print(i)
 
 
 
