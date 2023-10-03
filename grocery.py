5a062b9248bad018d8e0a4f2e34f5d3dce46b35c
Aly <aly_alidina@hotmail.com>
2023-10-03 16:17:35 +0000

    Tue, Oct 3, 2023, 12:17 PM -04:00

--- grocery.py
+++ grocery.py
@@ -1,13 +1,13 @@
-grocery = []
+grocery = {}
 count = 0
 while True:
     try:
         items = input().upper()
         grocery.append(items)
-        for i in grocery:
-            if i in grocery:
-                count +=1
-                grocery.append(count)
+        # for i in grocery:
+        if i in grocery:
+            count +=1
+            grocery.append(count)
     except EOFError:
             return str(count, i, end='\n')
 
