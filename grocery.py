f835dd128a61f3f8bbdf106a26ab470cc3939a93
Aly <aly_alidina@hotmail.com>
2023-10-03 16:38:08 +0000

    Tue, Oct 3, 2023, 12:38 PM -04:00

--- grocery.py
+++ grocery.py
@@ -5,6 +5,8 @@ while True:
         item = input("").upper()
         if item in grocery:
             grocery[item] += 1
+        else:
+            grocery[item] = 1
     except EOFError:
         for i in sorted(grocery):
             print(grocery[i], i)
