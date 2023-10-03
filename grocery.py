d4037d5c46cbf1c646d0e66a07c057bd9f814dfb
Aly <aly_alidina@hotmail.com>
2023-10-03 16:19:02 +0000

    Tue, Oct 3, 2023, 12:19 PM -04:00

--- grocery.py
+++ grocery.py
@@ -6,8 +6,8 @@ while True:
         grocery.append(items)
         # for i in grocery:
         if i in grocery:
-            count +=1
-            grocery.append(count)
+            # count +=1
+            # grocery.append(count)
     except EOFError:
             return str(count, i, end='\n')
 
