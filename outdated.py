01bbe74a12f767b3bf28e4fd12f0ba4cd5eb67bc
Aly <aly_alidina@hotmail.com>
2023-10-04 18:07:56 +0000

    Wed, Oct 4, 2023, 2:07 PM -04:00

--- outdated.py
+++ outdated.py
@@ -14,14 +14,10 @@ months = [
 ]
 try:
     #prompt user for date
-    date = input("Enter Date month day year or MM/DD/YYYY: ").upper().split('/')
-    m,d,y = date
-    print(y + "-" + m + "-" + d)
+    date = input("Enter Date month day year or MM/DD/YYYY: ").upper()
+    if "/" in date:
+        date.split("/")
+        m,d,y = date
+        print(y + "-" + m + "-" + d)
 except:
-    break
-
-    # for m in months:
-    # if m.upper() == months:
-    #     print("x")
-        # else:
-        #     print("y")
+    pass
