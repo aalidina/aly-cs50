4bdec0b5d73c5007aa7cf54da8a8e81f765120bb
Aly <aly_alidina@hotmail.com>
2023-10-04 17:35:38 +0000

    Wed, Oct 4, 2023, 1:35 PM -04:00

--- outdated.py
+++ outdated.py
@@ -17,6 +17,6 @@ months = [
 date = input("Enter Date month day year or MM/DD/YYYY: ").split()
 m,d,y = date
 if m in months:
-    print(y + "-" + m + "-" + d)
+    print(y,m,d)
 else:
     print(y + "-" + m + "-" + d)
