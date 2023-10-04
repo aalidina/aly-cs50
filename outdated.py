2bb903a59131bb718ff305fa5139b06924f1eac5
Aly <aly_alidina@hotmail.com>
2023-10-04 17:09:38 +0000

    Wed, Oct 4, 2023, 1:09 PM -04:00

---
+++ outdated.py
@@ -0,0 +1,17 @@
+[
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+]
+
+#prompt user for date
+date = input("Enter Date: ")
