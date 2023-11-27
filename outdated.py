0b8f895252605b734f20c814e445b8ba6cc7527e
Aly <aly_alidina@hotmail.com>
2023-11-27 18:36:43 +0000

    Mon, Nov 27, 2023, 1:36 PM -05:00

--- outdated.py
+++ outdated.py
@@ -28,10 +28,12 @@ while True:
             old_month,old_day,old_year = date.split(",")
             for i in range(len(months)):
                 if old_month == months[i]:
-                    month = (i + 1)
+                    month = i + 1
             day = old_day.replace(",", "")
             if (int(month) >= 1 and int(month) <= 12) and (int(day) >= 1 and int(day) <= 31):
                 break
         except:
             print()
             pass
+
+    print(f"{year}-{int(month):02}-{int(day):02}")
