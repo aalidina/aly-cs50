9106c47ccd6202df76f1188bf0bcbb864f71286a
Aly <aly_alidina@hotmail.com>
2023-11-27 18:27:27 +0000

    Mon, Nov 27, 2023, 1:27 PM -05:00

--- outdated.py
+++ outdated.py
@@ -12,15 +12,26 @@ months = [
     "November",
     "December"
 ]
-try:
-    #prompt user for date
-    date = input("Enter Date month day year or MM/DD/YYYY: ").upper()
-    if "/" in date:
-        m,d,y = date.split("/")
-        print(y + "-" + m + "-" + d)
-    elif "," in date:
-        m,d,y = date.split(",")
-        m = (months.index(m) + 1)
-        print(m)
-except:
-    pass
+
+#prompt user for date
+while True:
+    date = input("Enter Date month day year or MM/DD/YYYY: ")
+    try:
+        if "/" in date:
+            month,day,year = date.split("/")
+            year = year.replace(" ","")
+            # Check if month is between 1 and 12 and day is between 1 and 31
+            if (int(month) >= 1 and int(month) <= 12) and (int(day) >= 1 and int(day) <= 31)
+            break
+    except:
+        try:
+            old_month,old_day,old_year = date.split(",")
+            for i in range(len(months)):
+                if old_month == months[i]:
+                    month = (i + 1)
+            day = old_day.replace(",", "")
+            if (int(month) >= 1 and int(month) <= 12) and (int(day) >= 1 and int(day) <= 31)
+            break
+        except:
+            print()
+            pass
