a8b9034d31ea848f34eb977a3fa4bf3842a32358
Aly <aly_alidina@hotmail.com>
2023-11-27 18:28:26 +0000

    Mon, Nov 27, 2023, 1:28 PM -05:00

--- outdated.py
+++ outdated.py
@@ -21,8 +21,8 @@ while True:
             month,day,year = date.split("/")
             year = year.replace(" ","")
             # Check if month is between 1 and 12 and day is between 1 and 31
-            if (int(month) >= 1 and int(month) <= 12) and (int(day) >= 1 and int(day) <= 31)
-            break
+            if (int(month) >= 1 and int(month) <= 12) and (int(day) >= 1 and int(day) <= 31):
+                break
     except:
         try:
             old_month,old_day,old_year = date.split(",")
@@ -30,8 +30,8 @@ while True:
                 if old_month == months[i]:
                     month = (i + 1)
             day = old_day.replace(",", "")
-            if (int(month) >= 1 and int(month) <= 12) and (int(day) >= 1 and int(day) <= 31)
-            break
+            if (int(month) >= 1 and int(month) <= 12) and (int(day) >= 1 and int(day) <= 31):
+                break
         except:
             print()
             pass
