f92e28e216c18f60d35a99c859321572a014f280
Aly <aly_alidina@hotmail.com>
2023-12-01 19:52:49 +0000

    Fri, Dec 1, 2023, 2:52 PM -05:00

--- outdated.py
+++ outdated.py
@@ -1,4 +1,3 @@
-# Create list with the name of all months
 months = [
     "January",
     "February",
@@ -13,36 +12,28 @@ months = [
     "November",
     "December"
 ]
-# Loop forever
+
+#prompt user for date
 while True:
-    # Get user input
-    date = input("Date: ")
+    date = input("Enter Date month day year or MM/DD/YYYY: ")
     try:
-        # Split the date by /
-        month, day, year = date.split("/")
-        year= year.replace(" ", "")
-        # Check if month is in between of 1 and 12 and day between 1 and 31
+        month,day,year = date.split("/")
+        year = year.replace(" ","")
+        # Check if month is between 1 and 12 and day is between 1 and 31
         if (int(month) >= 1 and int(month) <= 12) and (int(day) >= 1 and int(day) <= 31):
             break
     except:
         try:
-            # Split the date by space
-            old_month, old_day, year = date.split(" ")
-            # Find the number of the month
+            old_month,old_day,year = date.split(" ")
             for i in range(len(months)):
                 if old_month == months[i]:
                     month = i + 1
             # Remove comma from day variable
-            day = old_day.replace(",","")
-            # Check if month is in between of 1 and 12 and day between 1 and 31
+            day = old_day.replace(",", "")
             if (int(month) >= 1 and int(month) <= 12) and (int(day) >= 1 and int(day) <= 31):
                 break
         except:
-            # Go to the next line
             print()
             pass
 
-# If month is less than 10, add a 0 before
-# If day is less than 10, add a 0 before
-# Print the result
 print(f"{year}-{int(month):02}-{int(day):02}")
