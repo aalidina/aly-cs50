6ea1f51b7f92f207363f9494b409b870d43bab50
Aly <aly_alidina@hotmail.com>
2023-12-01 20:00:56 +0000

    Fri, Dec 1, 2023, 3:00 PM -05:00

--- outdated.py
+++ outdated.py
@@ -24,6 +24,7 @@ while True:
             if (int(month) >= 1 and int(month) <= 12) and (int(day) >= 1 and int(day) <= 31):
                 break
         except:
+            pass
             try:
                 old_month,old_day,year = date.split(" ")
                 for i in range(len(months)):
