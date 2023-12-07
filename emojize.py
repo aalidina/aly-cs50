8e4aaafd4c61e3f63e742f8c94d778c428566476
Aly <aly_alidina@hotmail.com>
2023-12-07 19:44:50 +0000

    Thu, Dec 7, 2023, 2:44 PM -05:00

--- emojize.py
+++ emojize.py
@@ -2,7 +2,7 @@ import emoji
 
 input = input("Input: ")
 
-print(emoji.emojize("Output:" + (input)), language='en')
-print(emoji.emojize('Python es :pulgar_hacia_arriba:', language='es'))
+print(emoji.emojize("Output:" + (input), language='en'))
+
 
 
