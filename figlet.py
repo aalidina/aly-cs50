771edba5e4aedecb0de980206573f18a62d855c6
Aly <aly_alidina@hotmail.com>
2023-12-26 17:23:00 +0000

    Tue, Dec 26, 2023, 12:23 PM -05:00

--- figlet.py
+++ figlet.py
@@ -5,7 +5,14 @@ if sys.argv[1] not in ["-f","--font"]:
     sys.exit("Invalid usage")
 
 input = input("Enter Text: ")
+
+
+# // Initialize figlet modules
 figlet = Figlet()
 figlet.getFonts()
 
+# // Set the figlet font
+figlet.setFont(font = sys.argv[2])
 
+# // Print the rendered text
+print(figlet.renderText(inp))
