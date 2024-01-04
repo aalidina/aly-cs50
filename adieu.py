20cc3ed13d1187aa2e9baee31ba9abb42e3990cf
Aly <aly_alidina@hotmail.com>
2024-01-04 19:24:11 +0000

    Thu, Jan 4, 2024, 2:24 PM -05:00

--- adieu.py
+++ adieu.py
@@ -3,16 +3,12 @@ import sys, inflect
 p = inflect.engine()
 x = []
 
-while 1:
+while True:
     try:
         names = input("Name:")
-
-        if len(names) < 1:
-            sys.exit(0)
-
         x.append(names)
-
     except EOFError:
         print(f"\nAdieu, adieu, to {p.join(x)}")
-
         break
+
+
