dc55acc67fe2ff0338b5b94f39ab19837d256303
Aly <aly_alidina@hotmail.com>
2024-01-04 22:17:53 +0000

    Thu, Jan 4, 2024, 5:17 PM -05:00

--- game.py
+++ game.py
@@ -5,9 +5,10 @@ while x <= 0:
     number = int(input("Level: "))
     y = random.randint(1, 100)
     guess = int(input("Guess: "))
-    if guess < y:
-        print("Too small!")
-    elif guess > y:
-        print("Too large!")
-    else:
-        print("Just right!")
+    While guess > 0:
+        if guess < y:
+            print("Too small!")
+        elif guess > y:
+            print("Too large!")
+        else:
+            print("Just right!")
