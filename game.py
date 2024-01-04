8628bf4fb85b02e75aec36fce1a2889ef2db6443
Aly <aly_alidina@hotmail.com>
2024-01-04 21:51:39 +0000

    Thu, Jan 4, 2024, 4:51 PM -05:00

--- game.py
+++ game.py
@@ -1,4 +1,6 @@
 
-while number < 0:
-    number = input("Enter a positive integrer: ")
+x = 0
+while x <= 0:
+    number = input.int("Enter a positive integrer: ")
+    x + number
     print(number)
