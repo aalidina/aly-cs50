25016f4104d7aabdcee1cc177770b9bba96384d9
Aly <aly_alidina@hotmail.com>
2024-01-04 19:15:11 +0000

    Thu, Jan 4, 2024, 2:15 PM -05:00

--- adieu.py
+++ adieu.py
@@ -6,10 +6,8 @@ x = []
 while True:
     try:
         names = input("Name:")
-        if len(_names) < 1:
-            sys.exit(0)
         x.append(names)
 
     except EOFError:
         print(f"\nAdieu, adieu, to {p.join(x)}")
-        break
+        # break
