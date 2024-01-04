38b4510b8162d86dbed7836fd89c9a548eca0818
Aly <aly_alidina@hotmail.com>
2024-01-04 18:58:13 +0000

    Thu, Jan 4, 2024, 1:58 PM -05:00

--- adieu.py
+++ adieu.py
@@ -5,4 +5,5 @@ while True:
     names = input("Name:")
     x.append(names)
     print("Adieu, adieu, to", x[0])
+    print(f"\nAdieu, adieu, to {p.join(names)}")
     print(inflect("Output:" + (input), language='alias'))
