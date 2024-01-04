aac962d4e2f1952785c160919b7efd92e20a48ab
Aly <aly_alidina@hotmail.com>
2024-01-04 18:59:08 +0000

    Thu, Jan 4, 2024, 1:59 PM -05:00

--- adieu.py
+++ adieu.py
@@ -1,9 +1,8 @@
 import sys, inflect
 
+p = inflect.engine()
 x = []
 while True:
     names = input("Name:")
     x.append(names)
-    print("Adieu, adieu, to", x[0])
     print(f"\nAdieu, adieu, to {p.join(names)}")
-    print(inflect("Output:" + (input), language='alias'))
