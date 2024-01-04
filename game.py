a9f818069f5c2844d90d1e9e4a48889929e13db3
Aly <aly_alidina@hotmail.com>
2024-01-04 22:25:20 +0000

    Thu, Jan 4, 2024, 5:25 PM -05:00

--- game.py
+++ game.py
@@ -8,10 +8,10 @@ while x <= 0:
 
     while True:
         if guess > 0 and guess < level:
-            
-        if guess < y:
-            print("Too small!")
-        elif guess > y:
-            print("Too large!")
-        else:
-            print("Just right!")
+
+            if guess < y:
+                print("Too small!")
+            elif guess > y:
+                print("Too large!")
+            else:
+                print("Just right!")
