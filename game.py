0931ff071f1e9666ee6a4f946e2e5dbdd0b3b0ce
Aly <aly_alidina@hotmail.com>
2024-01-04 22:09:13 +0000

    Thu, Jan 4, 2024, 5:09 PM -05:00

--- game.py
+++ game.py
@@ -5,7 +5,7 @@ while x <= 0:
     number = int(input("Enter a positive integrer: "))
     y = random.randint(1, 100)
     guess = input("Guess: ")
-    if guess == y:
-        print 
+    if guess > y:
+        print
     x = number
     print(y)
