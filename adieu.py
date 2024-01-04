25259770f1b85dc02a334832a1b215591165f05f
Aly <aly_alidina@hotmail.com>
2024-01-04 21:39:45 +0000

    Thu, Jan 4, 2024, 4:39 PM -05:00

--- adieu.py
+++ adieu.py
@@ -1,14 +1,14 @@
 import sys, inflect
 
 p = inflect.engine()
-x = []
+list = []
 
 while True:
     try:
         names = input("Name:")
-        x.append(names)
+        list.append(names)
     except EOFError:
-        print(f"\nAdieu, adieu, to {p.join(x)}")
+        print(f"\nAdieu, adieu, to {p.join(list)}")
         break
 
 
