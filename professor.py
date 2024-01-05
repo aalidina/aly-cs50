dc17da6b3818df5f93dc47e9cdb9e305ca109a07
Aly <aly_alidina@hotmail.com>
2024-01-05 18:19:35 +0000

    Fri, Jan 5, 2024, 1:19 PM -05:00

--- professor.py
+++ professor.py
@@ -7,8 +7,9 @@ while level >= 1 and level <= 3:
     y = rand_num = random.randint(1, 9)
 
 i = 0
-while i < 10:
+while i < 9:
     input = int(input(f"{x} + {y} = "))
-
+    i += 1
+    print(i)
 result = x+y
 
