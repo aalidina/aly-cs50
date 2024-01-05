0aa6428ba1170aa11aa0734b34f6803190c64d56
Aly <aly_alidina@hotmail.com>
2024-01-05 17:32:14 +0000

    Fri, Jan 5, 2024, 12:32 PM -05:00

--- game.py
+++ game.py
@@ -1,39 +1,27 @@
 import random
 
-# // Get the level of the guessing game
-level: int = -1
+level : int = -1
 while level < 0:
     try:
         level = int(input("Level: "))
-    except Exception:
+    except ValueError:
         pass
 
-
-# // Generate a random number and set
-# // the current guess to a negative
-rand_num: int = random.randint(0, level)
+rand_num = random.randint(0, level)
 guess: int = -1
 
-# // While the guess is incorrect...
 while 1:
     try:
-        # // Get the users guess
-        guess: int = int(input("Guess: "))
+        guess = int(input("Guess: "))
 
-        # // If valid guess
         if guess > 0 and guess < level:
 
-            # // Print guess proximity
-            if guess < rand_num:
+            if guess <  rand_num:
                 print("Too small!")
-
-            elif guess > rand_num:
+            elif guess >  rand_num:
                 print("Too large!")
-
-            elif guess == rand_num:
+            elif guess ==  rand_num:
                 print("Just right!")
                 break
-
-    # // If an error occurs, ignore it
     except Exception:
         pass
