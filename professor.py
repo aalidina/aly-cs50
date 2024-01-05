cc7a1cb8068786d83267b80edefd8a4c14a6c4e0
Aly <aly_alidina@hotmail.com>
2024-01-05 17:58:17 +0000

    Fri, Jan 5, 2024, 12:58 PM -05:00

--- professor.py
+++ professor.py
@@ -3,4 +3,7 @@ import random
 level = 1
 while level >= 1 and level <= 3:
     user = input("Level: ")
-    rand_num = random.randint(1, 9)
+    x = rand_num = random.randint(1, 9)
+    y = rand_num = random.randint(1, 9)
+    print( x, y)
+    input = input("{x} + {y} = ")
