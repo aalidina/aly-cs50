7b575da557a674be51bf0f393b18ae64ed3de4ad
Aly <aly_alidina@hotmail.com>
2024-01-05 19:45:07 +0000

    Fri, Jan 5, 2024, 2:45 PM -05:00

--- professor.py
+++ professor.py
@@ -3,29 +3,35 @@ import random
 level : int = 0
 score = 0
 fail = 0
-while level <= 1 or level > 3:
-    try:
-        level = int(input("Level: "))
-    except Exception: pass
-return level
+
+def main()
+    while level <= 1 or level > 3:
+        
+        try:
+            level = int(input("Level: "))
+        except Exception:
+            pass
+            return level
 
 
 
-    i = 0
-    while i < 10:
+        i = 0
+        while i < 10:
 
-        x = rand_num = random.randint(1, 9)
-        y = rand_num = random.randint(1, 9)
-        math = int(input(f"{x} + {y} = "))
-        result = x+y
-        i += 1
+            x = rand_num = random.randint(1, 9)
+            y = rand_num = random.randint(1, 9)
+            math = int(input(f"{x} + {y} = "))
+            result = x+y
+            i += 1
 
-        if math == result:
-            score += 1
-        elif math != result:
-            fail += 1
-            print("EEE")
+            if math == result:
+                score += 1
+            elif math != result:
+                fail += 1
+                print("EEE")
 
-    break
+        break
     print(f"total score {score}")
+
+main()
 
