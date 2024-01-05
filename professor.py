873a006fd4c3f878afaac65d26d34eb0b0dbcaae
Aly <aly_alidina@hotmail.com>
2024-01-05 19:07:28 +0000

    Fri, Jan 5, 2024, 2:07 PM -05:00

--- professor.py
+++ professor.py
@@ -1,6 +1,8 @@
 import random
 
 level = 1
+right = 0
+wrong = 0
 while level >= 1 and level <= 3:
     user = int(input("Level: "))
 
@@ -8,8 +10,6 @@ while level >= 1 and level <= 3:
     i = 0
 
     while i < 10:
-        right = 0
-        wrong = 0
 
         x = rand_num = random.randint(1, 9)
         y = rand_num = random.randint(1, 9)
