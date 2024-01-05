24293a7ddcfa207bc7a9f8e933ad1c60cb247617
Aly <aly_alidina@hotmail.com>
2024-01-05 19:23:15 +0000

    Fri, Jan 5, 2024, 2:23 PM -05:00

--- professor.py
+++ professor.py
@@ -1,28 +1,27 @@
 import random
 
 level = 1
-right = 0
-wrong = 0
+score = 0
+fail = 0
 while level >= 1 and level <= 3:
     user = int(input("Level: "))
 
 
     i = 0
-    while i < 1:
+    while i < 10:
 
         x = rand_num = random.randint(1, 9)
         y = rand_num = random.randint(1, 9)
         math = int(input(f"{x} + {y} = "))
-        result = int(x+y)
+        result = x+y
         i += 1
 
         if math == result:
-            right += 1
-            print(right)
+            score += 1
         elif math != result:
-            wrong += 1
-        print(f"total score {right}")
-        print(right)
+            fail += 1
+
+        print(f"total score {score}")
     break
 
 
