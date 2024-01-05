a89c566ef23684fe51cb3f3d402ca88c2e88ffd7
Aly <aly_alidina@hotmail.com>
2024-01-05 18:04:25 +0000

    Fri, Jan 5, 2024, 1:04 PM -05:00

--- professor.py
+++ professor.py
@@ -5,5 +5,7 @@ while level >= 1 and level <= 3:
     user = input("Level: ")
     x = rand_num = random.randint(1, 9)
     y = rand_num = random.randint(1, 9)
-    
+
     input = input(f"{x} + {y} = ")
+    if x + y == input:
+        
