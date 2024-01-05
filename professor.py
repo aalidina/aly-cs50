403e19703fe0e9e6e51bc7dff180cdd09e0ceb30
Aly <aly_alidina@hotmail.com>
2024-01-05 18:46:36 +0000

    Fri, Jan 5, 2024, 1:46 PM -05:00

--- professor.py
+++ professor.py
@@ -6,19 +6,20 @@ while level >= 1 and level <= 3:
 
 
     i = 0
-
+    right = 0
+    wrong = 0
     while i < 10:
-        right = 0
-        wrong = 0
+
         x = rand_num = random.randint(1, 9)
         y = rand_num = random.randint(1, 9)
         math = input(f"{x} + {y} = ")
         result = x+y
         i += 1
-        if math == result:
-            right += 1
-        elif math != result:
-            wrong += 1
+        
+    if math == result:
+        right += 1
+    elif math != result:
+        wrong += 1
     break
 print(f"total score {right}")
 print(right)
