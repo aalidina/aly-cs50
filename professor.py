b6aee43aab92040e781553feb816b44691690f14
Aly <aly_alidina@hotmail.com>
2024-01-05 18:24:08 +0000

    Fri, Jan 5, 2024, 1:24 PM -05:00

--- professor.py
+++ professor.py
@@ -3,13 +3,15 @@ import random
 level = 1
 while level >= 1 and level <= 3:
     user = int(input("Level: "))
-    x = rand_num = random.randint(1, 9)
-    y = rand_num = random.randint(1, 9)
+
 
     i = 0
     while i < 9:
+        x = rand_num = random.randint(1, 9)
+        y = rand_num = random.randint(1, 9)
         math = input(f"{x} + {y} = ")
         i += 1
         print(i)
+        break
     result = x+y
 
