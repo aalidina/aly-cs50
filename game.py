0ac0b3b2aaa416f989f985e26a5621243e9438a7
Aly <aly_alidina@hotmail.com>
2024-01-25 18:50:20 +0000

    Thu, Jan 25, 2024, 1:50 PM -05:00

--- game.py
+++ game.py
@@ -2,26 +2,23 @@ import random
 
 level : int = -1
 while level < 0:
-    try:
-        level = int(input("Level: "))
-    except ValueError:
-        pass
 
-rand_num = random.randint(0, level)
-guess: int = -1
+    level = int(input("Level: "))
+
+    rand_num = random.randint(0, level)
+    guess: int = -1
 
 while True:
-    try:
-        guess = int(input("Guess: "))
 
-        if guess > 0 and guess < level:
+    guess = int(input("Guess: "))
 
-            if guess <  rand_num:
-                print("Too small!")
-            elif guess >  rand_num:
-                print("Too large!")
-            elif guess ==  rand_num:
-                print("Just right!")
-                break
-    except Exception:
-        pass
+    if guess > 0 and guess < level:
+
+        if guess <  rand_num:
+            print("Too small!")
+        elif guess >  rand_num:
+            print("Too large!")
+        elif guess ==  rand_num:
+            print("Just right!")
+            break
+
