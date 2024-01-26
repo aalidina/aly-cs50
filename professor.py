5a08ccae56b889ef939b8e05991152449215abed
Aly <aly_alidina@hotmail.com>
2024-01-26 19:20:57 +0000

    Fri, Jan 26, 2024, 2:20 PM -05:00

--- professor.py
+++ professor.py
@@ -17,23 +17,23 @@ while choice <= 1 or choice > 3:
 
 # def main():
 # level: int = get_level()
-i = 0
-while i < 10:
+    i = 0
+    while i < 10:
 
-    x = rand_num = random.randint(1, 9)
-    y = rand_num = random.randint(1, 9)
-    math = int(input(f"{x} + {y} = "))
-    result = x+y
-    i += 1
+        x = rand_num = random.randint(1, 9)
+        y = rand_num = random.randint(1, 9)
+        math = int(input(f"{x} + {y} = "))
+        result = x+y
+        i += 1
 
-    if math == result:
-        score += 1
-    elif math != result:
-        fail += 1
-        print("EEE")
+        if math == result:
+            score += 1
+        elif math != result:
+            fail += 1
+            print("EEE")
 
-    break
-print(f"total score {score}")
+        break
+    print(f"total score {score}")
 
 
 # main()
