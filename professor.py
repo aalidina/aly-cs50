7574cc6892ead9d6bb6dcb1b4c85028fed396980
Aly <aly_alidina@hotmail.com>
2024-01-26 20:32:34 +0000

    Fri, Jan 26, 2024, 3:32 PM -05:00

--- professor.py
+++ professor.py
@@ -18,7 +18,6 @@ def main():
             elif math != result:
                 fail += 1
         except ValueError:
-            i += 1
             print("EEE")
             pass
     print(f"total score {score}")
