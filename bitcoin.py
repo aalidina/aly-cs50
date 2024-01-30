ff650fa1e8ae47e492f31cf4d6593fd815cbd372
Aly <aly_alidina@hotmail.com>
2024-01-30 19:29:12 +0000

    Tue, Jan 30, 2024, 2:29 PM -05:00

--- bitcoin.py
+++ bitcoin.py
@@ -4,13 +4,14 @@ import requests
 number = float(input("Enter number of Bitcoin: "))
 
 try:
-    if number == " ":
-        print("Missing command-line argument")
-    elif number != int:
+    if number != int:
         print("Command-line argument is not a number")
-    else:
+    elif number == int:
         amount = number * 38761.0833
         print(f"${amount:,.4f}")
+    else:
+        raise ValueError
 except requests.RequestException:
     print("Missing Command Line Argument")
 
+# print("Missing Command Line Argument")
