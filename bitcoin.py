8fed2001d057983cbda9414770e468af30c28741
Aly <aly_alidina@hotmail.com>
2024-01-30 19:14:24 +0000

    Tue, Jan 30, 2024, 2:14 PM -05:00

--- bitcoin.py
+++ bitcoin.py
@@ -1,4 +1,7 @@
 
 number = float(input("Enter number of Bitcoin: "))
-amount = number * 38761.0833
-print(f"${amount:,.4f}")
+try:
+    amount = number * 38761.0833
+    print(f"${amount:,.4f}")
+except:
+    print()
