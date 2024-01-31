78b55648d98390e72aba816885971b5ece18c771
Aly <aly_alidina@hotmail.com>
2024-01-31 19:54:49 +0000

    Wed, Jan 31, 2024, 2:54 PM -05:00

--- bitcoin.py
+++ bitcoin.py
@@ -2,27 +2,28 @@
 import requests
 import sys
 
+print(sys.argv[0])
 
-
-
+if sys.argv[1] not in ["-f", "-font"]:
+     print("Missing Command Line Argument")
 
 
 # if type(number) != int:
 #     print("Command-line argument is not a number")
 # elif number == "":
 #     print("Missing Command Line Argument")
-try:
-    number = float(input("Enter number of Bitcoin: "))
-    # print(sys.argv[1])
+# try:
+#     number = float(input("Enter number of Bitcoin: "))
+#     # print(sys.argv[1])
 
-    if type(number) != float:
-         sys.exit("Command-line argument is not a number")
-    else:
-        amount = number * 38761.0833
-        print(f"${amount:,.4f}")
+#     if type(number) != float:
+#          sys.exit("Command-line argument is not a number")
+#     else:
+#         amount = number * 38761.0833
+#         print(f"${amount:,.4f}")
 
 
-except (ValueError, requests.RequestException):
-     sys.exit("Command-line argument is not a number")
+# except (ValueError, requests.RequestException):
+#      sys.exit("Command-line argument is not a number")
 
 
