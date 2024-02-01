9ce872a06211f7d3cc3ad584f4d7dc43dcd4ef94
Aly <aly_alidina@hotmail.com>
2024-02-01 18:27:40 +0000

    Thu, Feb 1, 2024, 1:27 PM -05:00

--- bitcoin.py
+++ bitcoin.py
@@ -13,19 +13,19 @@ from sys import argv
 
 
 def main():
-     value = req_coin()
-     if len(sys.argv) <= 1:
-          sys.exit("Missing Command Line Argument")
+     value = req_coin() # Assigning the value of req_coin to value
+     if len(sys.argv) <= 1: # Checking if an argument was passed when running python.bitcon.py
+          sys.exit("Missing Command Line Argument") #Exit if no argument was passed
 
      try:
-          amount = float(sys.argv[1]) * value
+          amount = float(sys.argv[1]) * value #multiple the number of bitcoin by bitcoin current price
           print(f"${amount:,.4f}")
-     except ValueError:
+     except ValueError: # If there is a ValueError exist the program and print the message.
           sys.exit("Command-line argument is not a number")
 
 
 def req_coin():
-     req = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
+     req = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json") # calling rest api to get bitcoin value
      data = req.json()
      usd = data["bpi"]["USD"]["rate_float"]
      return usd
