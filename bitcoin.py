893f3712f4948f0183279eeb1aee095ebf58563b
Aly <aly_alidina@hotmail.com>
2024-02-01 22:10:54 +0000

    Thu, Feb 1, 2024, 5:10 PM -05:00

--- bitcoin.py
+++ bitcoin.py
@@ -5,16 +5,9 @@ import json
 from sys import argv
 
 
-
-
-# result = float(usd) * float(gcoin)
-# #     print(gcoin)
-# #     print(f"${result:,.4f}")
-
-
 def main():
      value = req_coin() # Assigning the value of req_coin to value
-     if len(sys.argv) <= 1: # Checking if an argument was passed when running python.bitcon.py
+     if len(sys.argv) < 2: # Checking if an argument was passed when running python.bitcon.py
           sys.exit("Missing Command Line Argument") #Exit if no argument was passed
 
      try:
@@ -35,22 +28,6 @@ main()
 
 
 
-# if type(number) != int:
-#     print("Command-line argument is not a number")
-# elif number == "":
-#     print("Missing Command Line Argument")
-# try:
-#     number = float(input("Enter number of Bitcoin: "))
-#     # print(sys.argv[1])
 
-#     if type(number) != float:
-#          sys.exit("Command-line argument is not a number")
-#     else:
-#         amount = number * 38761.0833
-#         print(f"${amount:,.4f}")
-
-
-# except (ValueError, requests.RequestException):
-#      sys.exit("Command-line argument is not a number")
 
 
