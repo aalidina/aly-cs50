3eeeca69f869de83f0b0680efec10ea5fb4d866e
Aly <aly_alidina@hotmail.com>
2024-02-20 17:55:01 +0000

    Tue, Feb 20, 2024, 12:55 PM -05:00

--- twttr.py
+++ twttr.py
@@ -1,10 +1,18 @@
-vowels = ["a","e","i","o","u"]
-input = input("Input: ")
-for c in input:
-    if c in vowels:
-       print("", end="")
-    else:
-        print(c, end="")
-print(" ")
+
+def main():
+    shorten()
+
+def shorten(word):
+    vowels = ["a","e","i","o","u"]
+    input = input("Input: ")
+    for c in input:
+        if c in vowels:
+            print("", end="")
+        else:
+            print(c, end="")
+    print(" ")
+
+if __name__ == "__main__":
+    main()
 
 
