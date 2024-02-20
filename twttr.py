a68eaf035425630f6d019d8f16133d49c65e141f
Aly <aly_alidina@hotmail.com>
2024-02-20 18:14:39 +0000

    Tue, Feb 20, 2024, 1:14 PM -05:00

--- twttr.py
+++ twttr.py
@@ -4,7 +4,7 @@ def main():
 
 def shorten(word):
     vowels = ["a","e","i","o","u"]
-    input1 = input("Input: ")
+    input1 = input("Input: ").lowercase
     for c in input1:
         if c in vowels:
             print("", end="")
