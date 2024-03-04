6e268af4e0916fda6d6ccfab48688b6980f6e3e1
Aly <aly_alidina@hotmail.com>
2024-03-04 18:27:43 +0000

    Mon, Mar 4, 2024, 1:27 PM -05:00

--- bank.py
+++ bank.py
@@ -1,11 +1,17 @@
 from bank import value
 
 def main():
+    value(greeting)
+    
+def value(greeting):
+    greeting = (input("Greeting: ")).lower().strip()
+    if greeting.startswith("hello"):
+        print("$0")
+    elif greeting.startswith("h"):
+        print("$20")
+    else:
+        print("$100")
 
-greeting = (input("Greeting: ")).lower().strip()
-if greeting.startswith("hello"):
-    print("$0")
-elif greeting.startswith("h"):
-     print("$20")
-else:
-    print("$100")
+
+if __name__ == "__main__":
+    main()
