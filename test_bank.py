84216486a73dd08666e4e9c7bcfd7524179d4f19
Aly <aly_alidina@hotmail.com>
2024-03-04 18:39:42 +0000

    Mon, Mar 4, 2024, 1:39 PM -05:00

--- test_bank.py
+++ test_bank.py
@@ -5,6 +5,7 @@ def main():
 
 
 def test_twttr():
+    assert value("hello") == "$0"
     assert value("Hello, 20") == "$20"
     assert value("Hello, 100") == "$100"
     assert value("Cat, 100") == "$100"
