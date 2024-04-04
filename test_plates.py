1fca866fedfb873d8a7f7a89bf87301c4d6502be
Aly <aly_alidina@hotmail.com>
2024-04-04 18:02:31 +0000

    Thu, Apr 4, 2024, 2:02 PM -04:00

--- test_plates.py
+++ test_plates.py
@@ -6,15 +6,20 @@ def main():
 
 def test_is_valid():
     # “All vanity plates must start with at least two letters.”
-    assert is_valid("CS50") == True
+
     assert is_valid("50") == False
     assert is_valid("H") == False
 
+    # “Numbers cannot be used in the middle of a plate; they must come at the end.
+    #  For example, AAA222 would be an acceptable … vanity plate; AAA22A would not be acceptable.
+    # The first number used cannot be a ‘0’.”
+    assert is_valid("CS50") == True
+    
     # “… vanity plates may contain a maximum of 6 characters (letters or numbers) and a minimum of 2 characters.”
     assert is_valid("CS05") == False
     assert is_valid("CS50P") == False
      assert is_valid("OUTATIME") == False
-    
+
     # “No periods, spaces, or punctuation marks are allowed.”
     assert is_valid("PI3.14") == False
 
