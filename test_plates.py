b6209ece50b93c955043b4840fca9f44f553cc72
Aly <aly_alidina@hotmail.com>
2024-04-04 18:04:00 +0000

    Thu, Apr 4, 2024, 2:04 PM -04:00

--- test_plates.py
+++ test_plates.py
@@ -14,11 +14,11 @@ def test_is_valid():
     #  For example, AAA222 would be an acceptable … vanity plate; AAA22A would not be acceptable.
     # The first number used cannot be a ‘0’.”
     assert is_valid("CS50") == True
-    
-    # “… vanity plates may contain a maximum of 6 characters (letters or numbers) and a minimum of 2 characters.”
     assert is_valid("CS05") == False
     assert is_valid("CS50P") == False
-     assert is_valid("OUTATIME") == False
+
+    # “… vanity plates may contain a maximum of 6 characters (letters or numbers) and a minimum of 2 characters.”
+    assert is_valid("OUTATIME") == False
 
     # “No periods, spaces, or punctuation marks are allowed.”
     assert is_valid("PI3.14") == False
