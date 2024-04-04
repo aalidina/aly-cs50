53b9a259e7c642101b09dc748c58dc75bd0ee0a7
Aly <aly_alidina@hotmail.com>
2024-04-04 18:00:45 +0000

    Thu, Apr 4, 2024, 2:00 PM -04:00

--- test_plates.py
+++ test_plates.py
@@ -5,10 +5,17 @@ def main():
 
 
 def test_is_valid():
+    # “All vanity plates must start with at least two letters.”
     assert is_valid("CS50") == True
+    assert is_valid("50") == False
+    assert is_valid("H") == False
+
+    # “… vanity plates may contain a maximum of 6 characters (letters or numbers) and a minimum of 2 characters.”
     assert is_valid("CS05") == False
     assert is_valid("CS50P") == False
+     assert is_valid("OUTATIME") == False
+    
+    # “No periods, spaces, or punctuation marks are allowed.”
     assert is_valid("PI3.14") == False
-    assert is_valid("H") == False
-    assert is_valid("OUTATIME") == False
-    assert is_valid("50") == False
+
+
