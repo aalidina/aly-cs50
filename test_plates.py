b6eaecd5d50c397fbb5a1bac475299dc3b5be63b
Aly <aly_alidina@hotmail.com>
2024-04-04 17:45:00 +0000

    Thu, Apr 4, 2024, 1:45 PM -04:00

--- test_plates.py
+++ test_plates.py
@@ -6,8 +6,8 @@ def main():
 
 def test_is_valid():
     assert is_valid("AAA22A") == False
-    assert is_valid("Valid") == True
-    # assert is_valid("hi world") == 20
+    assert is_valid("AAA222") == True
+    assert is_valid("hi world") == 20
     # assert is_valid("HI WORLD") == 20
     # assert is_valid("wassup world") == 100
     # assert is_valid("WASSUP WORLD") == 100
